fe137dd8a585c6c7fc87172a04021ab11b92426b
Mark Tiddy <marktiddy@gmail.com>
2020-04-28 19:47:12 +0100

    Redesign

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Some script to implement smooth scrolling on all browsers
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on("click", function(event) {
+  $("a").on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -15,10 +15,10 @@ $(document).ready(function() {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $("html, body").animate(
         {
-          scrollTop: $(hash).offset().top
+          scrollTop: $(hash).offset().top,
         },
         800,
-        function() {
+        function () {
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         }
@@ -27,15 +27,13 @@ $(document).ready(function() {
   });
 });
 
-$(document).ready(function(event) {
+$(document).ready(function (event) {
   $(".navbar-brand").hide();
 });
 
 //Let's try and capture the data spy
-$(window).scroll(function(event) {
-  var scrolled_val = $(document)
-    .scrollTop()
-    .valueOf();
+$(window).scroll(function (event) {
+  var scrolled_val = $(document).scrollTop().valueOf();
 
   if (
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
@@ -51,11 +49,11 @@ $(window).scroll(function(event) {
 function logoDisplay(val, param) {
   if (val > param) {
     $(".navbar-brand").show({
-      duration: 200
+      duration: 200,
     });
   } else {
     $(".navbar-brand").hide({
-      duration: 200
+      duration: 200,
     });
   }
 }
